e020ca883f3af7ce047bee89405f35daf6ee1e2c
MRRomenin <roma_sozontov@mail.ru>
2025-04-22 11:41:34 +0600

    Push new project

---
+++ projectDDS/DDS/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, '../templates/index.html')
+
+def create_entry(request):
+    return render(request, 'create_entry.html')
+
+# Create your views here.
