7de700829fc579ff274c3fd8f18f1b010386fe4f
MRRomenin <roma_sozontov@mail.ru>
2025-04-22 12:02:31 +0600

    Add new html page

--- projectDDS/DDS/views.py
+++ projectDDS/DDS/views.py
@@ -9,4 +9,7 @@ def index(request):
 def create_entry(request):
     return render(request, 'create_entry.html')
 
+def manager(request):
+    return render(request, 'manager.html')
+
 # Create your views here.
